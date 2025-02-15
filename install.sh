ae1d3e0ae24cf366c8d0b227100f36012c034f5a
Karu <arthur.lassausaie@epitech.eu>
2025-02-15 02:28:30 +0100

    feat: adding more files

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
-sudo pacman -S hyprland neovim kitty tmux cliphist thunar otf-font-awesome ttf-space-mono-nerd rofi btop
+sudo pacman -S hyprland neovim kitty tmux cliphist thunar otf-font-awesome ttf-space-mono-nerd rofi btop zsh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+chsh -s $(which zsh)
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 git clone https://aur.archlinux.org/yay.git
-cd yay makepkg -si
+cd yay
+makepkg -si
+cd ..
 
 yay -S brave visual-studio-code-bin swaylock-effects swaybg waypaper waybar
 
 cp -r ./config/* $HOME/.config/
+cp -r ./.repos/* $HOME/.repos/
