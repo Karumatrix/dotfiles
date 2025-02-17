85e4c3588f408044105eae53d255a9eed5540773
Karu <arthur.lassausaie@epitech.eu>
2025-02-17 17:25:49 +0100

    feat: adding emoji font

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo pacman -S hyprland neovim kitty cliphist thunar otf-font-awesome ttf-space-mono-nerd rofi btop zsh ripgrep
+sudo pacman -S hyprland neovim kitty cliphist thunar otf-font-awesome ttf-space-mono-nerd rofi btop zsh ripgrep noto-fonts-emoji
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 chsh -s $(which zsh)
